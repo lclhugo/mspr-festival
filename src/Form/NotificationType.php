3bac025208ed58b61b0677624b4e845d94880ea9
lclhugo <hugo.laclau@gmail.com>
2023-05-15 17:04:55 +0200

    merge fixes

--- src/Form/NotificationType.php
+++ src/Form/NotificationType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Notification;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
