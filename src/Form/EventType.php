6847f1a5252261e52766f41e0fe871d9d260376f
jeromerobichez <jerome.robichez@gmail.com>
2023-05-15 16:01:26 +0200

    import form

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -7,7 +7,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
