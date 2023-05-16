41a54b299e09422a4794b184363b31b4280e7726
jeromerobichez <jerome.robichez@gmail.com>
2023-05-16 16:29:20 +0200

    start id festival

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\FestivalRepository;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(FestivalRepository $festivalRepository): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
-        ]);
+            'festivals' => $festivalRepository->findAll(),
+         ]);
     }
 }
