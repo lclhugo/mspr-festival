00a615a6fb4454167bc4ec8ff15dcbe4645ca4ee
lclhugo <hugo.laclau@gmail.com>
2023-05-17 16:02:29 +0200

    events

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,76 +3,83 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Entity\Festival;
 use App\Form\EventType;
 use App\Repository\EventRepository;
+use App\Repository\FestivalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/event')]
+#[Route('/admin/festival/{festivalId}/event')]
 class EventController extends AbstractController
 {
+    //get all events for a festival id
     #[Route('/', name: 'app_event_index', methods: ['GET'])]
-    public function index(EventRepository $eventRepository): Response
+    public function index($festivalId, EventRepository $eventRepository, FestivalRepository $festivalRepository): Response
     {
+
         return $this->render('event/index.html.twig', [
-            'events' => $eventRepository->findAll(),
+            'events' => $eventRepository->findBy(['festivalId' => $festivalId]),
+            'festival' => $festivalRepository->find($festivalId),
+            'festivalId' => $festivalId
         ]);
     }
 
     #[Route('/new', name: 'app_event_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EventRepository $eventRepository): Response
+    public function new($festivalId, Request $request, EventRepository $eventRepository, FestivalRepository $festivalRepository): Response
     {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $festival = $festivalRepository->find($festivalId);
+            $event->setFestivalId($festival);
             $eventRepository->save($event, true);
 
-            return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_event_index', ['festivalId' => $festivalId], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('event/new.html.twig', [
             'event' => $event,
             'form' => $form,
+            'festivalId' => $festivalId,
+            'festival' => $festivalRepository->find($festivalId)
         ]);
     }
 
     #[Route('/{id}', name: 'app_event_show', methods: ['GET'])]
-    public function show(Event $event): Response
+    public function show($festivalId, Event $event, EventRepository $eventRepository, FestivalRepository $festivalRepository): Response
     {
         return $this->render('event/show.html.twig', [
             'event' => $event,
+            'festivalId' => $festivalId,
+            'festival' => $festivalRepository->find($festivalId)
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_event_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Event $event, EventRepository $eventRepository): Response
+    public function edit(Request $request, Event $event, EventRepository $eventRepository, FestivalRepository $festivalRepository, $festivalId): Response
     {
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $festival = $festivalRepository->find($festivalId);
+            $event->setFestivalId($festival);
             $eventRepository->save($event, true);
 
-            return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_event_index', ['festivalId' => $festivalId], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('event/edit.html.twig', [
             'event' => $event,
             'form' => $form,
+            'festivalId' => $festivalId,
+            'festival' => $festivalRepository->find($festivalId)
         ]);
     }
 
-    #[Route('/{id}', name: 'app_event_delete', methods: ['POST'])]
-    public function delete(Request $request, Event $event, EventRepository $eventRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->request->get('_token'))) {
-            $eventRepository->remove($event, true);
-        }
-
-        return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
-    }
 }
